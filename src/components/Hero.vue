abce4fbdf88571f71fabdac28d6c0a058c021c1f
John Martin De Guia <jmartindeguia09@gmail.com>
2022-12-15 21:12:08 +0800

    features added: shortened links & copy to clipboard

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,24 +1,58 @@
 <template>
   <main class="min-h-max bg-blue-600 px-4 pt-10 pb-16 text-center">
-    <h1 class="text-4xl font-bold text-white lg:text-5xl">shortenIt</h1>
-    <p class="mx-auto pt-5 pb-7 text-white md:w-8/12 md:text-lg lg:w-1/2">
-      shortenIt is a free tool to shorten a URL or reduce a link. Create a short link with our URL shortener to make it
-      simple to remember.
-    </p>
+    <section class="mx-auto max-w-4xl">
+      <h1 class="text-4xl font-bold text-white lg:text-5xl">shortenIt</h1>
+      <p class="mx-auto pt-5 pb-7 text-white md:w-2/3 md:text-lg">
+        shortenIt is a free tool to shorten a URL or reduce a link. Create a short link with our URL shortener to make it
+        simple to remember.
+      </p>
 
-    <form class="flex flex-col justify-center md:flex-row">
-      <input
-        type="url"
-        class="mb-2 h-10 w-full border-2 border-gray-200 px-4 focus:border-blue-500 focus:outline-none md:mb-0 md:h-12 md:w-1/2 md:rounded-l-md"
-        placeholder="Paste your link here"
-      />
-      <input
-        type="submit"
-        value="Shorten"
-        class="h-10 w-full cursor-pointer bg-blue-800 text-white transition duration-300 ease-in-out hover:bg-blue-900 md:h-12 md:w-1/6 md:rounded-r-md"
-      />
-    </form>
+      <form @submit.prevent="shortenUrl" class="md:grid md:grid-cols-12">
+        <input v-model="urlInput" type="url" class="url-input" placeholder="Paste your link here" />
+        <input type="submit" value="Shorten" class="submit-btn" />
+      </form>
+
+      <article v-if="loading" v-show="shortenedUrl">
+        <Icon icon="line-md:loading-loop" color="white" class="mx-auto pt-10 text-8xl" />
+      </article>
+      <article
+        v-if="shortenedUrl"
+        v-show="!loading"
+        class="mt-5 flex flex-col items-center justify-between rounded bg-white p-4 sm:flex-row"
+      >
+        <p class="mb-3 font-semibold sm:mb-0">{{ shortenedUrl }}</p>
+        <button @click="copy(shortenedUrl)" class="copy-btn w-full sm:w-auto">
+          <span v-if="!copied">Copy to Clipboard</span>
+          <span v-else>Copied!</span>
+        </button>
+      </article>
+    </section>
   </main>
 </template>
 
-<script setup></script>
+<script setup>
+import { Icon } from "@iconify/vue";
+import { useClipboard } from "@vueuse/core";
+import { ref } from "vue";
+
+const urlInput = ref("");
+const shortenedUrl = ref("");
+const loading = ref(false);
+
+const { copy, copied } = useClipboard({ text: shortenedUrl, legacy: true });
+
+const shortenUrl = async () => {
+  try {
+    loading.value = true;
+    const response = await fetch(`https://api.shrtco.de/v2/shorten?url=${urlInput.value}`);
+    const data = await response.json();
+    shortenedUrl.value = data.result.full_short_link;
+    urlInput.value = "";
+  } catch (error) {
+    console.log(error);
+    urlInput.value = "";
+  } finally {
+    loading.value = false;
+  }
+};
+</script>
